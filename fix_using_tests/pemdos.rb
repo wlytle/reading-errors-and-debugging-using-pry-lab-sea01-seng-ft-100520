cec08dca4af74c89d5090865d481ed53207d8921
Will Lytle <wlytle89@gmail.com>
2020-08-10 21:18:21 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,12 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  snake_string = ""
   if string[0] == "s"
-    10 * "s" + string
+    10.times do
+      snake_string = snake_string + "s"
+    end
+    snake_string + string
   else
     string
   end
